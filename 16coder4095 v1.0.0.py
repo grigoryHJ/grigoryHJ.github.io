d5a2ce330ca1972555cf9e84738ecd49044cefbd
grigoryHJ <grigoriyhabluk@gmail.com>
2023-09-24 12:29:58 +0700

    16coder

---
+++ 16coder4095 v1.0.0.py
@@ -0,0 +1,167 @@
+i = int(input("Введите число: "))
+# 4095 last number
+while True:
+    if i < 16:
+        if i == 10:
+            i = "A"
+        if i == 11:
+            i = "B"
+        if i == 12:
+            i = "C"
+        if i == 13:
+            i = "D"
+        if i == 14:
+            i = "E"
+        if i == 15:
+            i = "F"
+        print(i)
+        break
+    i1 = i % 16
+    ii1 = i // 16
+    if ii1 == 10:
+        ii1 = "A"
+    if ii1 == 11:
+        ii1 = "B"
+    if ii1 == 12:
+        ii1 = "C"
+    if ii1 == 13:
+        ii1 = "D"
+    if ii1 == 14:
+        ii1 = "E"
+    if ii1 == 15:
+        ii1 = "F"
+    if i1 == 10:
+        i1 = "A"
+    if i1 == 11:
+        i1 = "B"
+    if i1 == 12:
+        i1 = "C"
+    if i1 == 13:
+        i1 = "D"
+    if i1 == 14:
+        i1 = "E"
+    if i1 == 15:
+        i1 = "F"
+    if ii1 == "A":
+        print(ii1, i1)
+        exit()
+    if ii1 == "B":
+        print(ii1, i1)
+        exit()
+    if ii1 == "C":
+        print(ii1, i1)
+        exit()
+    if ii1 == "D":
+        print(ii1, i1)
+        exit()
+    if ii1 == "E":
+        print(ii1, i1)
+        exit()
+    if ii1 == "F":
+        print(ii1, i1)
+        exit()
+    if ii1 == 0:
+        print(ii1, i1)
+        exit()
+    if ii1 == 1:
+        print(ii1, i1)
+        exit()
+    if ii1 == 2:
+        print(ii1, i1)
+        exit()
+    if ii1 == 3:
+        print(ii1, i1)
+        exit()
+    if ii1 == 4:
+        print(ii1, i1)
+        exit()
+    if ii1 == 5:
+        print(ii1, i1)
+        exit()
+    if ii1 == 6:
+        print(ii1, i1)
+        exit()
+    if ii1 == 7:
+        print(ii1, i1)
+        exit()
+    if ii1 == 8:
+        print(ii1, i1)
+        exit()
+    if ii1 == 9:
+        print(ii1, i1)
+        exit()
+    break
+i2 = ii1 % 16
+ii2 = ii1 // 16
+if ii2 == 10:
+    ii2 = "A"
+if ii2 == 11:
+    ii2 = "B"
+if ii2 == 12:
+    ii2 = "C"
+if ii2 == 13:
+    ii2 = "D"
+if ii2 == 14:
+    ii2 = "E"
+if ii2 == 15:
+    ii2 = "F"
+if i2 == 10:
+    i2 = "A"
+if i2 == 11:
+    i2 = "B"
+if i2 == 12:
+    i2 = "C"
+if i2 == 13:
+    i2 = "D"
+if i2 == 14:
+    i2 = "E"
+if i2 == 15:
+    i2 = "F"
+if ii2 == "A":
+    print(ii2, i2, i1)
+    exit()
+if ii2 == "B":
+    print(ii2, i2, i1)
+    exit()
+if ii2 == "C":
+    print(ii2, i2, i1)
+    exit()
+if ii2 == "D":
+    print(ii2, i2, i1)
+    exit()
+if ii2 == "E":
+    print(ii2, i2, i1)
+    exit()
+if ii2 == "F":
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 0:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 1:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 2:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 3:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 4:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 5:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 6:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 7:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 8:
+    print(ii2, i2, i1)
+    exit()
+if ii2 == 9:
+    print(ii2, i2, i1)
+    exit()
